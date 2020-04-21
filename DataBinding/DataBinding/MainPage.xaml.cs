a6339abfb7e8af56626a1161c74d315e99a3f947
Omar-Belghaouti <bel_omar18@yahoo.com>
2020-04-21 22:12:22 +0200

    Add project files.

---
+++ DataBinding/DataBinding/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using DataBinding.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DataBinding
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        private Fruit fruit;
+        private ObservableCollection<string> fruits;
+        public MainPage()
+        {
+            InitializeComponent();
+            //fruit = new Fruit { Name = "Apple" };
+            //this.BindingContext = fruit;
+            fruits = new ObservableCollection<string>();
+            fruits.Add("Alfa");
+            fruits.Add("Bravo");
+            fruits.Add("Charlie");
+            fruits.Add("Delta");
+
+            list1.ItemsSource = fruits;
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            //fruit.Name = "Banana";
+            //list1.ItemsSource = new string[] { "Alfa", "Bravo", "Charlie", "Delta" };
+            fruits.Add("Echo");
+        }
+    }
+}
---
+++ DataBinding/DataBinding/Models/Fruit.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace DataBinding.Models
+{
+    public class Fruit : INotifyPropertyChanged
+    {
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set 
+            {
+                if (name != value)
+                {
+                    name = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+    }
+}
